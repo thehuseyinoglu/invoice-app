a779654ad70077dc76004914aecf67ba40c814d5
thehuseyinoglu <thehuseyinoglu@gmail.com>
2023-01-16 02:46:40 +0300

    Create global component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
 import "./assets/index.css"
-createApp(App).mount('#app')
+import appHeading from "./components/ui/appHeading.vue"
+
+const app = createApp(App)
+app.component("heading",appHeading)
+app.mount('#app')
