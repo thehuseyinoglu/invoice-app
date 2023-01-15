9c5ac204d1ca0e45beda9ab5d5d64454fd81a148
thehuseyinoglu <thehuseyinoglu@gmail.com>
2023-01-16 02:47:51 +0300

    Write save - edit - delete function

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,30 @@
 <template>
- <h1>hello</h1>
+  <div class="w-screen h-screen bg-gray-800 flex flex-row text-white items-start justify-center">
+    <AppSidebar :invoices="invoiceList" :editInvoice="editInvoice" :deleteInvoice="deleteInvoice" />
+    <InvoiceContent :saveInvoice="saveInvoice" :activeInvoice="state.activeInvoice" />
+  </div>
 </template>
+<script setup>
+import AppSidebar from "./components/appSidebar.vue"
+import InvoiceContent from "./components/InvoiceContent.vue"
+import { ref,reactive } from "vue";
+
+const invoiceList = ref([]);
+
+const state = reactive({
+  activeInvoice:null
+})
+
+const saveInvoice = (invoice) => {
+ 
+  invoiceList.value.push(invoice)
+}
+
+const editInvoice = (invoice)=>{
+state.activeInvoice = invoice
+}
+const deleteInvoice = (invoice) =>{
+  invoiceList.value = invoiceList.value.filter(i => i.id != invoice.id)
+  console.log(invoceList);
+}
+</script>
